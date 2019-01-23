6f39ad86faa49251a3f5b7676494dcadfdff53d3
Jakob Buis <jakob.buis@in10.nl>
2019-01-23 17:12:07 +0100

    fix naked routes (without a language)

--- src/Router.php
+++ src/Router.php
@@ -9,16 +9,19 @@ class Router
     public function multilanguage()
     {
         return function (array $attributes, Closure $callback) {
+            // Define the route group again, but without an language, which
+            // defaults to the default language
+            $this->group($attributes, $callback);
+
             // Define a route group for every language used with that language
             // code as the route prefix
             $languages = config('languages.supported-languages');
             foreach ($languages as $language) {
-                $attributes = array_merge($attributes, [
+                $this->group(array_merge($attributes, [
                     'prefix' => $language,
                     'as' => $language . '.',
                     'middleware' => 'set-language-from-route',
-                ]);
-                $this->group($attributes, $callback);
+                ]), $callback);
             }
         };
     }
