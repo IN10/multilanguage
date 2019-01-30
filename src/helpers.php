ca571fa240d08e260825cbdfb0733928e587c264
Jakob Buis <jakob.buis@in10.nl>
2019-01-30 10:29:31 +0100

    improved version of the translatedRoute helper that uses the currently set language when not explicitly specified

--- src/helpers.php
+++ src/helpers.php
@@ -5,11 +5,14 @@ if (!function_exists('translatedRoute')) {
      * Generate a URL to a route, taken into account translations
      */
     function translatedRoute(
-        string $language,
         string $route,
         array $parameters = [],
-        bool $absolute = true
+        bool $absolute = true,
+        ?string $language = null
     ) : string {
+        if ($language === null) {
+            $language = \Illuminate\Support\Facades\App::getLocale();
+        }
         $actualRoute = "{$language}.{$route}";
         return route($actualRoute, $parameters, $absolute);
     }
