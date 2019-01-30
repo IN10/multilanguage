68e33dae0736dc0a27c5d16c30054da75464e22e
Jakob Buis <jakob.buis@in10.nl>
2019-01-30 12:01:50 +0100

    move naked routes below language-qualified routes, to prevent errors in routing when routes subsume each other

--- src/Router.php
+++ src/Router.php
@@ -9,11 +9,6 @@ class Router
     public function multilanguage()
     {
         return function (array $attributes, Closure $callback) {
-            // Define the route group again, but without an language, which
-            // defaults to the default language
-            $this->group(array_merge($attributes, [
-                'middleware' => 'set-language-from-route',
-            ]), $callback);
 
             // Define a route group for every language used with that language
             // code as the route prefix
@@ -25,6 +20,12 @@ class Router
                     'middleware' => 'set-language-from-route',
                 ]), $callback);
             }
+
+            // Define the route group again, but without an language, which
+            // defaults to the default language
+            $this->group(array_merge($attributes, [
+                'middleware' => 'set-language-from-route',
+            ]), $callback);
         };
     }
 }
